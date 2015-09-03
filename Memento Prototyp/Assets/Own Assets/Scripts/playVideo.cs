8349d20e39a3bcf453a4e56dbb52d84823c6f129
Dimitri Hoffmann <dh053@hdm-stuttgart.de>
2015-09-03 22:38:42 +0200

    StartScreen mit fullscreen Video added

---
+++ Memento Prototyp/Assets/Own Assets/Scripts/playVideo.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playVideo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Handheld.PlayFullScreenMovie ("MementoIntro.mp4", Color.black, FullScreenMovieControlMode.Hidden);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
